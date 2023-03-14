ee1caf91cabe9f4655afa7e5cce8415f46a5d014
DiegoGervazoni <diegoo.gervazoni@hotmail.com>
2023-03-14 17:00:51 -0300

    First commit

---
+++ src/com/projetono/Main.java
@@ -0,0 +1,32 @@
+package com.projetono;
+
+public class Main {
+    public static void main(String[] args) {
+
+        No no1 = new No("Conteudo No1");
+
+        No no2 = new No("Conteudo No2");
+        no1.setProximoNo(no2);
+
+        No no3  = new No("Conteudo No3");
+        no2.setProximoNo(no3);
+
+        No no4 = new No("Conteudo No4");
+        no3.setProximoNo(no4);
+
+        //Temos: no1 -> no2 -> no3 -> no4 -> null;
+
+        System.out.println(no1);
+        System.out.println(no1.getProximoNo());
+        System.out.println(no2);
+
+        System.out.println(" ------------------------- ");
+
+        System.out.println(no1);
+        System.out.println(no1.getProximoNo());
+        System.out.println(no1.getProximoNo().getProximoNo());
+        System.out.println(no1.getProximoNo().getProximoNo().getProximoNo());
+        System.out.println(no1.getProximoNo().getProximoNo().getProximoNo().getProximoNo());
+
+    }
+}
